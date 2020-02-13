640aeb5e43101ff9f4a383f604cdd455e2bc0996
Ian Wilson <iwilsonq@gmail.com>
2020-02-12 21:22:31 -0800

    git integration

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p>I am Ian Wilson, a full stack developer that runs and enjoys music.</p>
+      <p>
+        I enjoy programming topics like JavaScript, Rust, ReasonML and GraphQL. I've
+        written some articles about these which can be viewed on my profile at
+        https://dev.to/iwilsonq or here on this website.
+      </p>
+      <p>
+        In the near future, I would like to get into making educational video
+        content as well.
+      </p>
+      <p>Here's a neat exit quote.</p>
+      <blockquote>
+        <b>You stay classy, San Diego</b> - <em>Ron Burgundy</em>
+      </blockquote>
+    </Layout>
+  );
+}
